7ab5dc1d40dcc9e375e4c2acf0b85f75c40d86ef
Vicente Quiroz <99621vicenteaqv@gmail.com>
2025-12-06 20:56:52 -0400

    feat: restructure App component to include ComponentSection and ComponentExample; add Badges component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import "./App.css";
 import "./index.css";
 import Button from "./components/ui/button";
+import {
+  ComponentExample,
+  ComponentSection,
+} from "./components/ComponentSection";
 
 function App() {
   return (
     <>
-      <Button variant="destructive">hola como estas</Button>
-      <Button variant="">hola como estas</Button>
-      <Button variant="outline">hola como estas</Button>
-      <Button variant="ghost">hola como estas</Button>
+      <ComponentSection
+        title="Botones"
+        description="Presentacion de todos los botones">
+
+        <ComponentExample
+          title="Diferentes Botones"
+          description="Algunos Botones">
+
+          <Button variant="destructive">hola como estas</Button>
+          <Button variant="">hola como estas</Button>
+          <Button variant="outline">hola como estas</Button>
+          <Button variant="ghost">hola como estas</Button>
+
+        </ComponentExample>
+      </ComponentSection>
     </>
   );
 }
 
 export default App;
+<ComponentExample title="Tamaños" description="Diferentes tamaños de texto">
+  <Badge className="text-xs">Extra Small</Badge>
+  <Badge className="text-sm">Small</Badge>
+  <Badge>Default</Badge>
+  <Badge className="text-base px-4 py-1">Large</Badge>
+</ComponentExample>;
---
+++ src/components/ui/badges.jsx
@@ -0,0 +1,5 @@
+function Badges (){
+    return(
+        <a></a>
+    );
+}
